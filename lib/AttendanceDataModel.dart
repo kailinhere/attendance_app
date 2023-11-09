6f5872cc0196591ce941a66730cca37f3af931d9
kailin0 <93082452+kailin0@users.noreply.github.com>
2023-11-09 22:14:10 +0800

    data listing, sort, time format

---
+++ lib/AttendanceDataModel.dart
@@ -0,0 +1,43 @@
+import 'package:intl/intl.dart';
+
+class AttendanceDataModel {
+  String? name;
+  String? phone;
+  DateTime? date;
+  String? dateStr;
+  String? duration;
+
+  AttendanceDataModel({this.name, this.phone, this.date});
+
+  AttendanceDataModel.fromJson(Map<String, dynamic> json)
+  {
+    name = json['user'];
+    phone = json['phone'];
+    String oriDate = json['check-in'];
+    date = DateFormat("yyyy-MM-dd HH:mm:ss").parse(oriDate);
+    dateStr = DateFormat("dd MMM yyyy, h:mm a").format(date!);
+    duration = getTimeDifference();    
+  }
+
+  String getTimeDifference() {
+    if (date != null) {
+      // Get the current time
+      DateTime now = DateTime.now();
+
+      // Calculate the difference between the date in the model and the current time
+      Duration difference = now.difference(date!);
+
+      if (difference.inSeconds < 60) {
+        return "Just now";
+      } else if (difference.inMinutes < 60) {
+        return "${difference.inMinutes} minute(s) ago";
+      } else if (difference.inHours < 24) {
+        return "${difference.inHours} hour(s) ago";
+      } else {
+        return "${difference.inDays} days ago"; // For older dates, just display the date
+      }
+    } else {
+      return "Date not available";
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
+import 'package:attendance_app/AttendanceDataModel.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' as rootBundle;
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,33 +18,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Attendance Records'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +35,139 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<AttendanceDataModel> attendanceData = [];
+  bool isDescending = true;
+  bool isDuration = true;
+  String timeText = "";
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    loadSharedPrefData();
+    _fetchData();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Column(children: [
+          Row(
+            children: [
+              IconButton(
+                  onPressed: () => setState(() {
+                        isDescending = !isDescending;
+                        saveSharedPrefData();
+                      }),
+                  icon: Icon(
+                      isDescending ? Icons.arrow_downward : Icons.arrow_upward,
+                      size: 28)),
+              IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isDuration = !isDuration;
+                      saveSharedPrefData();
+                    });
+                  },
+                  icon: Icon(isDuration
+                      ? Icons.access_time_filled
+                      : Icons.calendar_today))
+            ],
+          ),
+          Expanded(
+              child: ListView.builder(
+                  itemCount: attendanceData.length,
+                  itemBuilder: (context, index) {
+                    final sortedData = isDescending
+                        ? attendanceData
+                        : attendanceData.reversed.toList();
+
+                    return Card(
+                      elevation: 5,
+                      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                      child: Container(
+                          padding: EdgeInsets.all(8),
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(left: 8, right: 8),
+                                child: Text(
+                                  sortedData[index].name.toString(),
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left: 8, right: 8),
+                                child: Text(sortedData[index].phone.toString()),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left: 8, right: 8),
+                                child: isDuration
+                                    ? Text(
+                                        sortedData[index].duration.toString())
+                                    : Text(
+                                        sortedData[index].dateStr.toString()),
+                              )
+                            ],
+                          )),
+                    );
+                  })),
+          
+        ]),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              setState(() {});
+            },
+            child: Icon(Icons.add),
+            tooltip: 'Add record',
+          )
+        );
+  }
+
+  Future<List<AttendanceDataModel>> readJsonData() async {
+    final jsondata =
+        await rootBundle.rootBundle.loadString('data/attendance_list.json');
+    final list = json.decode(jsondata) as List<dynamic>;
+
+    return list.map((e) => AttendanceDataModel.fromJson(e)).toList();
+  }
+
+  Future<void> _fetchData() async {
+    List<AttendanceDataModel> data = await readJsonData();
+    setState(() {
+      attendanceData =
+          data.reversed.toList(); // Update the state with fetched data
+    });
+  }
+
+  void loadSharedPrefData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool savedIsDescending = prefs.getBool('isDescending') ?? true;
+    bool savedIsDuration = prefs.getBool('isDuration') ?? true;
+    setState(() {
+      isDescending = savedIsDescending;
+      isDuration = savedIsDuration;
+
+      print('isDescending start: $isDescending');
+      print('isDuration start: $isDuration');
+    });
+  }
+
+  void saveSharedPrefData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isDescending', isDescending);
+    prefs.setBool('isDuration', isDuration);
+
+    print('isDescending stop: $isDescending');
+    print('isDuration stop: $isDuration');
   }
 }
